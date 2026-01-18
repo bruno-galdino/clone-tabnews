80cca5104aaf3bd6f841ddcbba46d1ed75f58879
Bruno Soares Galdino <bsg.galdino@gmail.com>
2026-01-18 23:36:45 +0000

    Novas mudanças

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Esta é minha primeira página :)</h1>;
+  return (
+    <div>
+      <h1>Esta é minha primeira página :)</h1>
+      <input type="number"></input>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
